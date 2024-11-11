d5403d4d27e73909125439d1c14d9de47065010b
p-omartineza <p-omartineza@izzi.mx>
2024-11-10 20:29:50 -0700

    add socketEvent perdedores, terminar partida

--- src/interfaces/socket-response.ts
+++ src/interfaces/socket-response.ts
@@ -28,6 +28,8 @@ export enum SocketEvents {
   MOVER_FICHA_PAGANDO = 'mover_ficha_pagando',
   MOVER_FICHA_AUTOMATICO = 'mover_ficha_automatico',
   GANADOR = 'ganador',
+  PERDEDORES = 'perdedores',
+  TERMINAR_PARTIDA = 'terminar',
 }
 // Función para 200 OK
 export function ok<T>(
